3305e84d28e4ff04f4c2d94eccb8c9b4c5cc9aac
airpark69 <rudah69@gmail.com>
2023-03-27 11:07:35 +0900

    Update settings.py

--- p1/settings.py
+++ p1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-lr2w$8t%61rx^(&jmfh&%65mkypuc4gcx429*os%d8lm1%bfwa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-54-180-116-51.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-54-180-116-51.ap-northeast-2.compute.amazonaws.com', 'ELB-291304836.ap-northeast-2.elb.amazonaws.com']
 
 
 # Application definition
